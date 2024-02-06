49e96a97efe8b6c1a1ff0debd42c108d55b80f6c
SanjayRamesh31 <118175052+SanjayRamesh31@users.noreply.github.com>
2024-02-06 15:59:21 +0530

    Add files via upload

---
+++ Day9/src/main/java/com/agrocloud/sanjay/dto/response/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.agrocloud.sanjay.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String message;
+}
