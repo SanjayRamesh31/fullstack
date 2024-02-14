077c934a1201fa0ee463331c9b5891c1025e4532
SanjayRamesh31 <118175052+SanjayRamesh31@users.noreply.github.com>
2024-02-14 11:51:02 +0530

    Delete BACKEND directory

--- BACKEND/src/main/java/com/agrocloud/sanjay/enumerated/Role.java
+++
@@ -1,5 +0,0 @@
-package com.agrocloud.sanjay.enumerated;
-
-public enum Role {
-    ADMIN,USER
-}
--- BACKEND/src/main/java/com/agrocloud/sanjay/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agrocloud.sanjay.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.agrocloud.sanjay.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, String> {
-
-}
